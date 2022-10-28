bafe484e26cc364c7107bab9c7a7062c5dc94bd9
sheilarodrj <sheila.rodriguez@ulv.edu.mx>
2022-10-28 16:20:47 -0500

    final

--- promedios/promedios/Program.cs
+++ promedios/promedios/Program.cs
@@ -10,6 +10,34 @@ namespace promedios
     {
         static void Main(string[] args)
         {
-        }
+            int contadorcal = 0;
+            double calificacion = 0;
+            double sumatotal = 0;
+            double resultadopromedio = 0;
+            string texto = "";
+
+            do
+            {
+
+                Console.WriteLine("Por favor, ingrese una calificación para calcular el promedio: ");
+                texto = Console.ReadLine();
+                bool ingreso = double.TryParse(texto, out calificacion);
+
+
+                if (ingreso == true)
+
+                {
+                    sumatotal = sumatotal + calificacion;
+                    contadorcal++;
+                }
+
+                if (ingreso == false)
+                {
+                    Console.WriteLine("Ese no es un valor válido, por favor intente de nuevo: ");
+                }
+            } while (texto != "fin");
+              resultadopromedio = sumatotal/ contadorcal;
+            Console.WriteLine("El promedio es " + resultadopromedio);
+            Console.ReadKey();
     }
-}
+    } }
