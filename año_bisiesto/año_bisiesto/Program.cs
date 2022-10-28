a7d129a4d0f1e70ff7adcca66a2eef2e173e6569
sheilarodrj <sheila.rodriguez@ulv.edu.mx>
2022-10-28 01:55:21 -0500

    ...

---
+++ año_bisiesto/año_bisiesto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace año_bisiesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int año;
+                Console.WriteLine("Ingrese el año: ");
+                año = int.Parse(Console.ReadLine());
+
+
+                if (año % 4 == 0 && (año % 100 != 0 | año % 400 == 0))
+                {
+                    Console.WriteLine("El año: " + año + ", si es bisiesto ");
+                }
+                else
+                {
+                    Console.WriteLine("El año " + año + ", no es bisiesto ");
+                }
+                Console.ReadLine();
+
+                Console.WriteLine("Presione Enter para continuar evaluando años");
+                Console.ReadKey();
+            } while (true);
+        }
+    }
+}
