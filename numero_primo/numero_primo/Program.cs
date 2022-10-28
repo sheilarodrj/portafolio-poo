1f4d12db896915547f37475dd15035ca825ccad9
sheilarodrj <sheila.rodriguez@ulv.edu.mx>
2022-10-28 01:57:00 -0500

    ....

---
+++ numero_primo/numero_primo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numero_primo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int numero;
+                int contador = 0;
+
+                Console.WriteLine("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i < (numero + 1); i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        contador++;
+                    }
+                }
+                if (contador != 2)
+                {
+                    Console.WriteLine("El número " + numero + ", no es primo");
+                }
+                else
+                {
+                    Console.WriteLine("El número " + numero + ", es primo");
+                }
+
+                Console.ReadLine();
+
+                Console.WriteLine("Presione Enter para continuar evaluando números");
+                Console.ReadKey();
+            } while (true);
+        }
+    }
+}
