299928a410239f18d764d51e86b2cd00ad7a0dc9
sheilarodrj <sheila.rodriguez@ulv.edu.mx>
2022-10-28 01:51:30 -0500

    Ejercicio 3

---
+++ descuento/descuento/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace descuento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escritorioscant;
+            double precio;
+            double compra;
+            double descuento = 0;
+            double total;
+
+
+            Console.WriteLine("Número de escritorios: ");
+            escritorioscant = int.Parse(Console.ReadLine());
+
+            precio = 6500;
+            compra = escritorioscant * precio;
+
+            if (escritorioscant < 5)
+            {
+                descuento = compra * 0.1;
+            }
+            else
+                if (escritorioscant < 10)
+            {
+                descuento = compra * 0.2;
+            }
+            else
+                if (escritorioscant >= 10)
+            {
+                descuento = compra * 0.4;
+            }
+            total = compra - descuento;
+            Console.WriteLine("El valor a pagar es: " + total);
+        }
+    }
+}
